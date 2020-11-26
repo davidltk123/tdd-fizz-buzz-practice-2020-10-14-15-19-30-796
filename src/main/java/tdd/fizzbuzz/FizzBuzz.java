4ac03edcec52e863cab3e728038687f7a446fd7e
DAVID T K LEUNG (DEV-ISD-OOCLL/HKG) <LEUNGDA3@OOCL.COM>
2020-11-26 14:04:02 +0800

    [FizzBuzz][David Leung]refactor: add constant

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -7,19 +7,21 @@ public class FizzBuzz {
     private static final int modulo_7 = 7;
     private static final int modulo_15 = 15;
     private static final int MODULO_21 = 21;
+    private static final int MODULO_35 = 35;
     private static final String FIZZ = "Fizz";
     private static final String BUZZ = "Buzz";
     private static final String WHIZZ = "Whizz";
     private static final String FIZZ_BUZZ = "FizzBuzz";
     private static final String FIZZ_WHIZZ = "FizzWhizz";
+    private static final String BUZZ_WHIZZ = "BuzzWhizz";
 
     public String countOff(int order) {
         if (order % modulo_15 == 0) {
             return FIZZ_BUZZ;
         } else if (order % MODULO_21 == 0) {
             return FIZZ_WHIZZ;
-        } else if (order % 35 == 0) {
-            return "BuzzWhizz";
+        } else if (order % MODULO_35 == 0) {
+            return BUZZ_WHIZZ;
         } else if (order % modulo_3 == 0) {
             return FIZZ;
         } else if (order % modulo_5 == 0) {
